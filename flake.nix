e095464acae96a7d6178a68ea3e27a27d6a3fb61
David Baynard <git@baynard.dev>
2022-07-07 17:11:05 +0100

    Create flake with wrapper for packaging

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  description = "Miscellaneous custom packages";
+
+  outputs = { nixpkgs, ... }@inputs:
+    let
+      lib = nixpkgs.lib // { inherit foldMap forSystems mergeFlakeOutputs; };
+
+      foldMap = f:
+        builtins.foldl' (acc: x: lib.recursiveUpdate acc (f x)) { };
+
+      forSystems = lib.flip foldMap;
+
+      mergeFlakeOutputs =
+        foldMap (file: import file (inputs // { inherit lib; }));
+    in
+    mergeFlakeOutputs [
+    ] // { inherit lib; };
+}
