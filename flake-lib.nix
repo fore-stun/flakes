206f4a50cea266bc3f1f271624cd77da26f51237
David Baynard <git@baynard.dev>
2022-10-08 00:27:41 +0100

    Add my systems as explicit list

--- flake-lib.nix
+++ flake-lib.nix
@@ -1,12 +1,22 @@
 { nixpkgs }:
 
 let
-  lib = nixpkgs.lib // { inherit foldMap foldFor mkApp; };
+  lib = nixpkgs.lib.recursiveUpdate nixpkgs.lib {
+    inherit foldMap foldFor mkApp;
+    platforms = { inherit mySystems; };
+  };
 
   foldMap = f:
     builtins.foldl' (acc: x: lib.recursiveUpdate acc (f x)) { };
 
   foldFor = lib.flip foldMap;
+
+  mySystems = [
+    "x86_64-linux"
+    "aarch64-linux"
+    "x86_64-darwin"
+    "aarch64-darwin"
+  ];
 
   mkApp = program: { type = "app"; inherit program; };
 in
