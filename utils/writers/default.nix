e5404c842278a07833809e234f999c692812f4f2
David Baynard <git@baynard.dev>
2022-11-14 23:17:54 +0000

    Expose writers as overlay

--- utils/writers/default.nix
+++ utils/writers/default.nix
@@ -1,9 +1,15 @@
-{ lib, nixpkgs, ... }:
+{ self, lib, nixpkgs, ... }:
 
 let
   pnames = [ "writePythonBin" "writeZshBin" ];
 in
-lib.foldFor pnames (pname: lib.foldFor lib.platforms.all (system: {
-  packages.${system}.writers.${pname} =
-    nixpkgs.legacyPackages.${system}.callPackage (./. + "/${pname}.nix") { };
-}))
+{
+  overlays.writers = final: prev: lib.foldFor pnames (pname: {
+    writers.${pname} = prev.callPackage (./. + "/${pname}.nix") { };
+  });
+} //
+lib.foldFor lib.platforms.all (system: {
+  packages.${system} = self.overlays.writers
+    self.packages.${system}
+    nixpkgs.legacyPackages.${system};
+})
