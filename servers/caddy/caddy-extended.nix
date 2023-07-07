9ed8fa090d65fbb20038cc38c69e87a39c18ce8c
David Baynard <git@baynard.dev>
2023-07-07 01:47:10 +0100

    Use spanx source for caddy

--- servers/caddy/caddy-extended.nix
+++ servers/caddy/caddy-extended.nix
@@ -1,17 +1,26 @@
 { lib
 , caddy
 , buildGoModule
+, fetchFromGitHub
 }:
 
+let
+  rev = "cf5f5418d695a15e9888539e169fdd8dd64e0f77";
+
+  src = fetchFromGitHub {
+    name = "spanx-${rev}-src";
+    owner = "fore-stun";
+    repo = "spanx";
+    inherit rev;
+    hash = "sha256-mhUjtSpbIhpx6ghRK3EwiA7pkYeSePBULyOdpVQru3I=";
+  };
+
+in
 (caddy.override ({
   buildGoModule = args: buildGoModule (args // {
-    vendorHash = "sha256-M1VpxNQ1G4BOD/lmciiL1qylUKjrnucpi6LBfIi1+MY=";
-    patches = [
-      (builtins.path {
-        name = "caddy.patch";
-        path = ./caddy.patch;
-      })
-    ];
+    version = "2.6.4";
+    vendorHash = "sha256-PV2K929Sum6qLF8JwDKzxITp6qy24QoToU9Oh0xU8fE=";
+    inherit src;
   });
 })).overrideAttrs
   (old: {
