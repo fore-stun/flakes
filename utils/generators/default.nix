8be09079889b24f0a6d54a1e841f62447a394689
David Baynard <git@baynard.dev>
2024-02-25 02:52:45 +0000

    Handle extras in generators

--- utils/generators/default.nix
+++ utils/generators/default.nix
@@ -4,12 +4,18 @@ let
   pnames = [ "names" ];
 in
 {
-  overlays.generators = final: prev: lib.foldFor pnames (pname: {
-    ${pname} = prev.callPackage (./. + "/${pname}.nix") { };
-  });
+  overlays.generators = final: prev:
+    let
+      extras = { };
+    in
+    lib.foldFor pnames (pname: {
+      ${pname} = prev.callPackage
+        (./. + "/${pname}.nix")
+        (extras."${pname}" or { });
+    });
 } //
 lib.foldFor lib.platforms.all (system: {
   packages.${system} = self.overlays.generators
-    self.packages.${system}
+    (nixpkgs.legacyPackages.${system} // self.packages.${system})
     nixpkgs.legacyPackages.${system};
 })
