5b98de66bad4611209607b0d323291bb94eff5c1
David Baynard <git@baynard.dev>
2024-02-25 23:29:58 +0000

    WIP provide actual word list to an

--- utils/generators/an.nix
+++ utils/generators/an.nix
@@ -1,4 +1,5 @@
 { lib
+, aspell
 , fetchgit
 , hostPlatform
 , hunspellDicts
@@ -26,15 +27,25 @@ let
 in
 stdenv.mkDerivation {
   inherit pname version src meta;
-  nativeBuildInputs = [ icu ];
+  nativeBuildInputs = [ aspell hunspellDicts.en_GB-large icu ];
   buildInputs = [ makeWrapper ];
+  postBuild = ''
+    mkdir aspellData
+    ln -sv "${hunspellDicts.en_GB-large}/share/hunspell/en_GB.aff" aspellData/en_DB.dat
+    aspell --lang=en_GB --data-dir=aspellData \
+      --encoding=utf-8 expand < "${hunspellDicts.en_GB-large}/share/hunspell/en_GB.dic" \
+        | tr " " "\n"
+        > english_wordlist.txt
+  '';
   preInstall = ''
+    mkdir -p "$out/lib"
     mkdir -p "$out/bin"
     mkdir -p "$out/usr/share/man/man6"
   '';
   postInstall = ''
+    mv -v english_wordlist.txt "$out/lib/"
     wrapProgram "$out/bin/${pname}" \
-      --add-flags "--dict=${hunspellDicts.en_GB-large}/share/hunspell/en_GB.dic"
+      --add-flags "--dict=$out/lib/english_wordlist.txt"
   '';
   buildFlags = lib.optionals hostPlatform.isDarwin [ "CC=clang" ];
   installFlags = [ "DESTDIR=$(out)" "INSTALLDIR=$(out)/bin" ];
