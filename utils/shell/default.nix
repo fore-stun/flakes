ed11be9f403395fd9e313ca8ddd5b99d7d577049
David Baynard <git@baynard.dev>
2024-02-28 18:42:39 +0000

    Restrict jkparse to linux

--- utils/shell/default.nix
+++ utils/shell/default.nix
@@ -4,9 +4,10 @@ let
   pnames = [ "jkparse" ];
 in
 {
-  overlays.shell = final: prev: lib.foldFor pnames (pname: {
-    ${pname} = prev.callPackage (./. + "/${pname}.nix") { };
-  });
+  overlays.shell = final: prev: lib.foldFor pnames
+    (pname: {
+      ${pname} = prev.callPackage (./. + "/${pname}.nix") { };
+    });
 } //
 lib.foldFor lib.platforms.all (system: {
   packages.${system} = self.overlays.shell
