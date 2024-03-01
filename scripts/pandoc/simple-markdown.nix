b158b4feb6867175466adcc87dbb0395f5765a98
David Baynard <git@baynard.dev>
2024-03-01 07:18:49 +0000

    Add simple-markdown lua filter to split sentences
    This stringifies the input, identifies which spaces (in order)
    should be replaced with newlines, and then walks the original AST,
    replacing the relevant spaces with newlines.

--- scripts/pandoc/simple-markdown.nix
+++ scripts/pandoc/simple-markdown.nix
@@ -10,6 +10,9 @@ let
   version = "0.1.0";
 
   libraries = builtins.attrValues {
+    inherit (lua.pkgs)
+      LuaNLP
+      ;
   };
 
   initLua =
@@ -47,21 +50,94 @@ let
     }
   '';
 
+  split = writers.writeLuaBin lua "${pname}-split-filter.lua"
+    {
+      inherit libraries; doCheck = "lua54+pandoc";
+    } ''
+    local tokenization = require("tokenizer.tokenization")
+
+    local function patcher(toreplace)
+      local space = 0
+      local replacement = 1
+
+      local function patch(el)
+        space = space + 1
+        if space == toreplace[replacement] then
+          replacement = replacement + 1
+          return pandoc.Str("\n")
+        end
+        return el
+      end
+
+      return {
+        -- traverse = 'topdown',
+        Space = patch,
+        SoftBreak = patch,
+      }
+    end
+
+    local function trackspace(spaces)
+      local function track(el)
+        spaces[1] = spaces[1] + 1
+        return el
+      end
+
+      return {
+        traverse = "topdown",
+        Space = track,
+        SoftBreak = track,
+      }
+    end
+
+    local function tokenize(el)
+      local tokens = tokenization.sentence_tokenize(pandoc.utils.stringify(el.content))
+      local toreplace = pandoc.List()
+
+      local i = 0
+      for tok in tokens do
+        if i > 0 then
+          i = i + 1
+          toreplace:insert(i)
+        end
+        local spaces = { 0 }
+        pandoc.Inlines(tok):walk(trackspace(spaces))
+        i = i + spaces[1]
+      end
+
+      el.content = el.content:walk(patcher(toreplace))
+      return el
+    end
+
+    return {
+      {
+        Para = tokenize,
+        Plain = tokenize,
+      },
+    }
+  '';
+
   script = writers.writeZshBin "${pname}" ''
     convertPandoc() {
       zparseopts -D -E -F -- \
         -pandoc-extra-arg+:=pandoc_extra P+:=pandoc_extra \
         -grid-tables=opt_grid_tables G=opt_grid_tables \
+        -no-split=opt_no_split S=opt_no_split \
         -markdown=opt_markdown m=opt_markdown
 
       local FROM="$( (( #opt_markdown )) && echo "markdown" || echo "html" )"
       local GRID_TABLES="$( (( #opt_grid_tables )) && echo "" || echo "-grid_tables" )"
+
+      local no_split="$( (( #opt_no_split )) && echo "" || echo "-no_split" )"
 
       local -a PANDOC_ARGS=(
         -r''${FROM} -wmarkdown-smart-simple_tables-multiline_tables''${GRID_TABLES}
         --data-dir=${initLua}
         --wrap=none --lua-filter=${strip}
       )
+
+      if ! (( #opt_no_split )); then
+        PANDOC_ARGS+=(--lua-filter=${lib.getExe split})
+      fi
 
       local PANDOC_EXTRA_SIGIL=(--pandoc-extra-arg -P)
       PANDOC_ARGS+=("''${(@)pandoc_extra:|PANDOC_EXTRA_SIGIL}")
@@ -74,5 +150,5 @@ let
 in
 lib.standalone {
   inherit version script;
-  passthru = { inherit initLua strip; };
+  passthru = { inherit initLua strip split; };
 }
