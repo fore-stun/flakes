69946f6edb95a3564fb2bd5494c8174d335594a6
David Baynard <git@baynard.dev>
2024-03-15 06:04:30 +0000

    Use lib.subFlake for utils/email

--- utils/email/default.nix
+++ utils/email/default.nix
@@ -1,15 +1,5 @@
-{ self, lib, nixpkgs, ... }:
+{ ... }:
 
-let
-  pnames = [ "eml2html" ];
-in
 {
-  overlays.email = final: prev: lib.foldFor pnames (pname: {
-    ${pname} = prev.callPackage (./. + "/${pname}.nix") { };
-  });
-} //
-lib.foldFor lib.platforms.all (system: {
-  packages.${system} = self.overlays.email
-    self.packages.${system}
-    nixpkgs.legacyPackages.${system};
-})
+  eml2html = null;
+}
