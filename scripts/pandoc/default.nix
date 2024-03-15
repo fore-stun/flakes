053da489e18f14172fadd9d098a4b39a6f47f620
David Baynard <git@baynard.dev>
2024-03-15 06:10:27 +0000

    Use lib.subFlake for scripts/pandoc

--- scripts/pandoc/default.nix
+++ scripts/pandoc/default.nix
@@ -1,37 +1,10 @@
-{ self, lib, nixpkgs, ... }:
+{ ... }:
 
-let
-  pnames = [
-    "csvs-from-markdown-tables"
-    "simple-markdown"
-  ];
-in
 {
-  overlays.pandoc = final: prev:
-    let
-      extras = {
-        simple-markdown = {
-          lua = final.lua5_4;
-        };
-      };
-    in
-    lib.foldFor pnames (pname: {
-      ${pname} = prev.callPackage (./. + "/${pname}.nix") ({
-        inherit (final) writers;
-        inherit lib;
-      } // extras."${pname}" or { });
-    });
-} //
-lib.foldFor lib.platforms.all (system:
-  {
-    packages.${system} = self.overlays.pandoc
-      self.legacyPackages.${system}
-      nixpkgs.legacyPackages.${system};
-  } //
-  lib.foldFor pnames (pname: {
-    apps.${system}.${pname} = {
-      type = "app";
-      program = self.packages.${system}.${pname} + "/bin/${pname}";
+  "csvs-from-markdown-tables" = null;
+  "simple-markdown" = {
+    final = pkgs: {
+      lua = pkgs.lua5_4;
     };
-  })
-)
+  };
+}
