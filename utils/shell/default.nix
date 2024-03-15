c946b3ce39cee4cc6978a2d94e91d2b4ed375982
David Baynard <git@baynard.dev>
2024-03-15 06:05:04 +0000

    Use lib.subFlake for utils/shell

--- utils/shell/default.nix
+++ utils/shell/default.nix
@@ -1,16 +1,5 @@
-{ self, lib, nixpkgs, ... }:
+{ ... }:
 
-let
-  pnames = [ "jkparse" ];
-in
 {
-  overlays.shell = final: prev: lib.foldFor pnames
-    (pname: {
-      ${pname} = prev.callPackage (./. + "/${pname}.nix") { };
-    });
-} //
-lib.foldFor lib.platforms.all (system: {
-  packages.${system} = self.overlays.shell
-    self.packages.${system}
-    nixpkgs.legacyPackages.${system};
-})
+  jkparse = null;
+}
