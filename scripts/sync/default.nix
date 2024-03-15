ea26d2b715cd08e3ae2cd86ac7c1746bd825e4d2
David Baynard <git@baynard.dev>
2024-03-15 06:05:36 +0000

    Use lib.subFlake for scripts/sync

--- scripts/sync/default.nix
+++ scripts/sync/default.nix
@@ -1,26 +1,5 @@
-{ self, lib, nixpkgs, ... }:
+{ ... }:
 
-let
-  pnames = [ "twoway" ];
-in
 {
-  overlays.sync = final: prev: lib.foldFor pnames (pname: {
-    ${pname} = prev.callPackage (./. + "/${pname}.nix") {
-      inherit (final) writers;
-      inherit lib;
-    };
-  });
-} //
-lib.foldFor lib.platforms.all (system:
-  {
-    packages.${system} = self.overlays.sync
-      self.legacyPackages.${system}
-      nixpkgs.legacyPackages.${system};
-  } //
-  lib.foldFor pnames (pname: {
-    apps.${system}.${pname} = {
-      type = "app";
-      program = self.packages.${system}.${pname} + "/bin/${pname}";
-    };
-  })
-)
+  twoway = null;
+}
