5d8311774766de9ac62a6295ef047225cc1bcd47
David Baynard <git@baynard.dev>
2024-03-15 04:03:35 +0000

    Use lib.subFlake for exif

--- utils/exif/default.nix
+++ utils/exif/default.nix
@@ -1,18 +1,5 @@
-{ self, lib, nixpkgs, ... }:
+{ ... }:
 
-let
-  pnames = [ "exif-copy" ];
-in
 {
-  overlays.exif = final: prev: lib.foldFor pnames (pname: {
-    ${pname} = prev.callPackage (./. + "/${pname}.nix") {
-      inherit (final) writers;
-      inherit lib;
-    };
-  });
-} //
-lib.foldFor lib.platforms.all (system: {
-  packages.${system} = self.overlays.exif
-    self.legacyPackages.${system}
-    nixpkgs.legacyPackages.${system};
-})
+  "exif-copy" = null;
+}
