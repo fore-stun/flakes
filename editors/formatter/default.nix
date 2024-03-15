85ffc9ff40aae577d8417cd86fa52c5e7d9cab97
David Baynard <git@baynard.dev>
2024-03-15 08:15:04 +0000

    Use lib.subFlake for editors/formatter

--- editors/formatter/default.nix
+++ editors/formatter/default.nix
@@ -1,15 +1,5 @@
-{ self, lib, nixpkgs, ... }:
+{ ... }:
 
-let
-  pnames = [ "prettier-plugin-svelte" ];
-in
 {
-  overlays.formetter = final: prev: lib.foldFor pnames (pname: {
-    ${pname} = prev.callPackage (./. + "/${pname}.nix") { };
-  });
-} //
-lib.foldFor lib.platforms.all (system: {
-  packages.${system} = self.overlays.formetter
-    self.packages.${system}
-    nixpkgs.legacyPackages.${system};
-})
+  "prettier-plugin-svelte" = null;
+}
