9ebac322a504508f3e5e5c4b247c37b6602dbce3
David Baynard <git@baynard.dev>
2024-03-15 08:16:04 +0000

    Use lib.subFlake for deploy/org-formation

--- deploy/org-formation/default.nix
+++ deploy/org-formation/default.nix
@@ -1,16 +1,5 @@
-{ self, lib, nixpkgs, ... }:
+{ ... }:
 
-let
-  pname = "org-formation";
-  systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" ];
-in
 {
-  overlays.${pname} = final: prev: {
-    ${pname} = prev.callPackage ./package.nix { };
-  };
-} //
-lib.foldFor systems (system: {
-  packages.${system} = self.overlays.${pname}
-    self.packages.${system}
-    nixpkgs.legacyPackages.${system};
-})
+  "org-formation" = null;
+}
