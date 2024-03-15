7e6ee3152bacb88466cd20079cf12d83aa9e3e3b
David Baynard <git@baynard.dev>
2024-03-15 06:06:02 +0000

    Use lib.subFlake for scripts/text

--- scripts/text/default.nix
+++ scripts/text/default.nix
@@ -1,25 +1,5 @@
-{ self, lib, nixpkgs, ... }:
+{ ... }:
 
-let
-  pnames = [ "pysplit" ];
-in
 {
-  overlays.text = final: prev: lib.foldFor pnames (pname: {
-    ${pname} = prev.callPackage (./. + "/${pname}.nix") {
-      inherit (final) writers;
-    };
-  });
-} //
-lib.foldFor lib.platforms.all (system:
-  {
-    packages.${system} = self.overlays.text
-      self.legacyPackages.${system}
-      nixpkgs.legacyPackages.${system};
-  } //
-  lib.foldFor pnames (pname: {
-    apps.${system}.${pname} = {
-      type = "app";
-      program = self.packages.${system}.${pname} + "/bin/${pname}";
-    };
-  })
-)
+  pysplit = null;
+}
