736f2b852d0e5fb00dada74f384911fafc6b39f3
David Baynard <git@baynard.dev>
2024-03-15 08:11:46 +0000

    Use lib.subFlake for utils/generators

--- utils/generators/default.nix
+++ utils/generators/default.nix
@@ -1,25 +1,10 @@
-{ self, lib, nixpkgs, ... }:
+{ ... }:
 
-let
-  pnames = [ "an" "names" ];
-in
 {
-  overlays.generators = final: prev:
-    let
-      extras = {
-        an = {
-          inherit (final) hunspellDicts;
-        };
-      };
-    in
-    lib.foldFor pnames (pname: {
-      ${pname} = prev.callPackage
-        (./. + "/${pname}.nix")
-        (extras."${pname}" or { });
-    });
-} //
-lib.foldFor lib.platforms.all (system: {
-  packages.${system} = self.overlays.generators
-    (nixpkgs.legacyPackages.${system} // self.packages.${system})
-    nixpkgs.legacyPackages.${system};
-})
+  an = {
+    final = pkgs: {
+      inherit (pkgs) hunspellDicts;
+    };
+  };
+  names = null;
+}
