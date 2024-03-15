a458b3bc5122bf73b2a45b52bfdb44d7fe4659de
David Baynard <git@baynard.dev>
2024-03-15 06:03:38 +0000

    Use lib.subFlake for scripts/nix

--- scripts/nix/default.nix
+++ scripts/nix/default.nix
@@ -1,28 +1,5 @@
-{ self, lib, nixpkgs, ... }:
+{ ... }:
 
-let
-  pnames = [
-    "nix-load-systemd-unit"
-  ];
-in
 {
-  overlays.nix-script = final: prev: lib.foldFor pnames (pname: {
-    ${pname} = prev.callPackage (./. + "/${pname}.nix") {
-      inherit (final) writers;
-      inherit lib;
-    };
-  });
-} //
-lib.foldFor lib.platforms.all (system:
-  {
-    packages.${system} = self.overlays.nix-script
-      self.legacyPackages.${system}
-      nixpkgs.legacyPackages.${system};
-  } //
-  lib.foldFor pnames (pname: {
-    apps.${system}.${pname} = {
-      type = "app";
-      program = self.packages.${system}.${pname} + "/bin/${pname}";
-    };
-  })
-)
+  "nix-load-systemd-unit" = null;
+}
