219bb2bd30972359b0ddb7f2191a02e4c3dc9f83
David Baynard <git@baynard.dev>
2024-03-15 04:58:00 +0000

    Prepare to make pysplit standalone
    If this commit appears in your `git blame`, you need to run the
    following to configure the `.git-blame-ignore-revs` file.
    
    ```
    git config blame.ignoreRevsFile .git-blame-ignore-revs
    ```
    
    If you have done that, then please replace the erroneous commit in that
    file with the hash of this commit.

--- scripts/text/pysplit.nix
+++ scripts/text/pysplit.nix
@@ -2,30 +2,36 @@
 , python3Packages
 }:
 
-writers.writePythonBin "pysplit"
-{
-  libraries = builtins.attrValues
+let
+  pname = "pysplit";
+
+  script = writers.writePythonBin "${pname}"
     {
-      inherit (python3Packages) pysbd;
-    };
-} ''
-  from itertools import groupby
-  import sys
+      libraries = builtins.attrValues
+        {
+          inherit (python3Packages) pysbd;
+        };
+    } ''
+    from itertools import groupby
+    import sys
 
-  import pysbd
-
-
-  def main():
-      text = sys.stdin.read().splitlines()
-      seg = pysbd.Segmenter(language="en", clean=False)
-      for chunk in ("\n".join(l) for _, l in groupby(text, lambda x: x != "")):
-          if chunk.startswith("#"):
-              print(chunk)
-              continue
-          for t in seg.segment(chunk) or [""]:
-              print(t.rstrip())
+    import pysbd
 
 
-  if __name__ == "__main__":
-      main()
-''
+    def main():
+        text = sys.stdin.read().splitlines()
+        seg = pysbd.Segmenter(language="en", clean=False)
+        for chunk in ("\n".join(l) for _, l in groupby(text, lambda x: x != "")):
+            if chunk.startswith("#"):
+                print(chunk)
+                continue
+            for t in seg.segment(chunk) or [""]:
+                print(t.rstrip())
+
+
+    if __name__ == "__main__":
+        main()
+  '';
+
+in
+script
