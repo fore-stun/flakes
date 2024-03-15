69e3ab253a7f91499939199d88f74a35842e623a
David Baynard <git@baynard.dev>
2024-03-15 07:57:35 +0000

    Revert "Use lib.subFlake for utils/writers"
    This reverts commit eff84450d8305fa1ac45aee2f27e5e50d0931ebb.

--- utils/writers/default.nix
+++ utils/writers/default.nix
@@ -1,7 +1,17 @@
-{ ... }:
+{ self, lib, nixpkgs, ... }:
 
+let
+  pnames = [ "writeLuaBin" "writePythonBin" "writeZshBin" ];
+in
 {
-  writeLuaBin = null;
-  writePythonBin = null;
-  writeZshBin = null;
-}
+  overlays.writers = final: prev: {
+    writers = prev.writers or { } // lib.foldFor pnames (pname: {
+      ${pname} = prev.callPackage (./. + "/${pname}.nix") { };
+    });
+  };
+} //
+lib.foldFor lib.platforms.all (system: {
+  legacyPackages.${system} = self.overlays.writers
+    self.legacyPackages.${system}
+    nixpkgs.legacyPackages.${system};
+})
