eff84450d8305fa1ac45aee2f27e5e50d0931ebb
David Baynard <git@baynard.dev>
2024-03-15 06:08:43 +0000

    Use lib.subFlake for utils/writers

--- utils/writers/default.nix
+++ utils/writers/default.nix
@@ -1,17 +1,7 @@
-{ self, lib, nixpkgs, ... }:
+{ ... }:
 
-let
-  pnames = [ "writeLuaBin" "writePythonBin" "writeZshBin" ];
-in
 {
-  overlays.writers = final: prev: {
-    writers = prev.writers or { } // lib.foldFor pnames (pname: {
-      ${pname} = prev.callPackage (./. + "/${pname}.nix") { };
-    });
-  };
-} //
-lib.foldFor lib.platforms.all (system: {
-  legacyPackages.${system} = self.overlays.writers
-    self.legacyPackages.${system}
-    nixpkgs.legacyPackages.${system};
-})
+  writeLuaBin = null;
+  writePythonBin = null;
+  writeZshBin = null;
+}
