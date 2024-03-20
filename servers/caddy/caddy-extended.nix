893eb8a57e3227c637fdd2a3357c9fe5f66d676f
David Baynard <git@baynard.dev>
2024-03-20 17:35:59 +0000

    Update caddy-security

--- servers/caddy/caddy-extended.nix
+++ servers/caddy/caddy-extended.nix
@@ -4,7 +4,7 @@
 caddyWith {
   plugins = {
     "github.com/fore-stun/spanx" = "9e9109e6b964cd36fb9cb1be8328b0f32b3d60c3";
-    "github.com/greenpau/caddy-security" = "8d00b5c2ae849b997a9faad47fab9f92dfd77b08";
+    "github.com/greenpau/caddy-security" = "bdd7abe375e7b0c13e6233a2f9b5433ba69c6af9";
     "github.com/lindenlab/caddy-s3-proxy" = "850db193cb7f48546439d236f2a6de7bd7436e2e";
   };
   vendorHash = "sha256-TAPG66OdnPjsblcNb+M2KLMHhiLBBmbKb2Q6r51xTb4=";
