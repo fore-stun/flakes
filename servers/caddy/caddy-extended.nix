c273e7d778b69e1f9dc6eb9274380a8e6061b213
David Baynard <git@baynard.dev>
2024-03-20 17:35:50 +0000

    Update hash for caddy-extended vendor dir

--- servers/caddy/caddy-extended.nix
+++ servers/caddy/caddy-extended.nix
@@ -7,5 +7,5 @@ caddyWith {
     "github.com/greenpau/caddy-security" = "8d00b5c2ae849b997a9faad47fab9f92dfd77b08";
     "github.com/lindenlab/caddy-s3-proxy" = "850db193cb7f48546439d236f2a6de7bd7436e2e";
   };
-  vendorHash = "sha256-y7iq9v2KDc5N5W4JRJtnVeG3CpjzlGAUYo3Qk0IkjFk=";
+  vendorHash = "sha256-TAPG66OdnPjsblcNb+M2KLMHhiLBBmbKb2Q6r51xTb4=";
 }
