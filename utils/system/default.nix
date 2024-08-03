b8bb7f8abca4485612b3e0874e97641d4860ef56
David Baynard <git@baynard.dev>
2024-08-03 12:22:10 +0100

    Stub system scripts

---
+++ utils/system/default.nix
@@ -0,0 +1,19 @@
+{ self, lib, nixpkgs, ... }:
+
+let
+  pnames = [ ];
+in
+{
+  overlays.system-script = final: prev: lib.foldFor pnames
+    (pname: {
+      ${pname} = prev.callPackage (./. + "/${pname}.nix") {
+        inherit (final) writers;
+        inherit lib;
+      };
+    });
+} //
+lib.foldFor lib.platforms.all (system: {
+  packages.${system} = self.overlays.system-script
+    self.legacyPackages.${system}
+    nixpkgs.legacyPackages.${system};
+})
