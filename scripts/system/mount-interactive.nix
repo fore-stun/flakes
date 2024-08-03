6acdf84c3b343bc0de79b2e826d7223196f7614b
David Baynard <git@baynard.dev>
2024-08-03 12:34:09 +0100

    Add script to mount interactively on linux

---
+++ scripts/system/mount-interactive.nix
@@ -0,0 +1,47 @@
+{ lib
+, fzf
+, gawk
+, udisks
+, util-linuxMinimal
+, writers
+}:
+let
+  pname = "mount-interactive";
+  version = "0.1.0";
+  script = writers.writeZshBin "${pname}" ''
+    interactive_mount() {
+      local DEVICE
+      local MOUNTPOINT
+      ${util-linuxMinimal}/bin/lsblk -lnpb -o NAME,ID-LINK,MOUNTPOINT \
+        | ${fzf}/bin/fzf --reverse --ansi \
+          --preview-window=up,3 \
+          --preview='print -l -- {1} {2} {3}' \
+        | ${lib.getExe gawk} '{print $2, $3}' \
+        | { read -r DEVICE MOUNTPOINT || : }
+
+      if ! (( #DEVICE )); then
+        print -l -- "No device selected" >&2
+        return 0
+      fi
+
+      local CMD
+      if (( #MOUNTPOINT )); then
+        CMD="unmount"
+      else
+        CMD="mount"
+      fi
+
+      print -- "''${CMD}ing…" >&2
+
+      ${udisks}/bin/udisksctl "''${CMD}" -b "/dev/disk/by-id/''${DEVICE}"
+    }
+
+    interactive_mount
+  '';
+in
+lib.standalone {
+  inherit version script;
+  meta = {
+    platforms = lib.platforms.linux;
+  };
+}
