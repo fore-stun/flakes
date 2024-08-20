60cdabc004089dc6de7a6c6147b82a8fe9556c63
David Baynard <git@baynard.dev>
2024-08-20 01:58:37 +0100

    Abstract function to create python scope

--- dependencies/python/default.nix
+++ dependencies/python/default.nix
@@ -26,26 +26,9 @@ let
 
 in
 {
-  overlays.python = final: prev:
-    let
-      pythonPkgsScope = prev.python3.pkgs.overrideScope (prev.lib.composeManyExtensions [
-        (_: _: newPackages final prev)
-      ]);
-    in
-    {
-      python3 = prev.python3 // {
-        pkgs = pythonPkgsScope;
-        withPackages = f: prev.python3.buildEnv.override { extraLibs = f pythonPkgsScope; };
-
-        packageOverrides = lib.warn ''
-          `python3.packageOverrides` does not compose;
-          instead, manually replace the `pkgs` attr of `python3` with `python3.pkgs.overrideScope` applied to the overrides.
-        ''
-          prev.python3.packageOverrides;
-      };
-
-      python3Packages = pythonPkgsScope;
-    };
+  overlays.python = final: prev: lib.pythonScopeWith prev "3" [
+    (_: _: newPackages final prev)
+  ];
 
 } //
 lib.foldFor lib.platforms.all (system: {
--- lib/default.nix
+++ lib/default.nix
@@ -23,11 +23,31 @@ let
       passthru = old.passthru or { } // passthru;
     });
 
+  pythonScopeWith = prev: pythonVariant: overlays:
+    let
+      python3 = prev."python${pythonVariant}";
+      pkgs = python3.pkgs.overrideScope (lib.composeManyExtensions overlays);
+    in
+    {
+      "python${pythonVariant}" = python3 // {
+        inherit pkgs;
+        withPackages = f: python3.buildEnv.override { extraLibs = f pkgs; };
+
+        packageOverrides = lib.warn ''
+          `python3.packageOverrides` does not compose;
+          instead, manually replace the `pkgs` attr of `python3` with `python3.pkgs.overrideScope` applied to the overrides.
+        ''
+          prev.python3.packageOverrides;
+      };
+      "python${pythonVariant}Packages" = pkgs;
+    };
+
 in
 lib.recursiveUpdate lib {
   platforms = { inherit anyNix; };
   licenses = { inherit dual; };
   inherit
     standalone
+    pythonScopeWith
     ;
 }
