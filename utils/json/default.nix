be05d3a9846eedb26f53cbc3befa8edb4f6251bd
David Baynard <git@baynard.dev>
2025-02-05 17:40:54 +0000

    Stub json utils

---
+++ utils/json/default.nix
@@ -0,0 +1,22 @@
+{ self, lib, nixpkgs, ... }:
+
+let
+  pnames = [ ];
+in
+{
+  overlays.json = final: prev:
+    let
+      extras = { };
+    in
+    lib.foldFor pnames (pname: {
+      ${pname} = prev.callPackage
+        (./. + "/${pname}.nix")
+        (extras."${pname}" or { });
+    });
+} //
+lib.foldFor lib.platforms.all (system: {
+  packages.${system} = self.overlays.json
+    (nixpkgs.legacyPackages.${system} // self.packages.${system})
+    nixpkgs.legacyPackages.${system};
+})
+
