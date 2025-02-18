c4f0171fa5b9ae0df5cc31f708c5c80e88b08715
David Baynard <git@baynard.dev>
2025-02-18 13:02:06 +0000

    Upgrade org-formation to 1.0.14

--- deploy/org-formation/package.nix
+++ deploy/org-formation/package.nix
@@ -5,14 +5,14 @@
 
 let
   pname = "org-formation";
-  version = "v1.0.13";
+  version = "v1.0.14";
 
   src = fetchFromGitHub {
     name = "${pname}-${version}-src";
     owner = "fore-stun";
     repo = "${pname}-cli";
-    rev = "6187fd19094bb5e4139937efd7a82c14877d94c6";
-    hash = "sha256-7LE5uOVZa9GUMJMoqaPIRS9QAjuatc37ujEVC74JJ2E=";
+    rev = "42b7339db1e8bae567591b936b4d1a67dfd8bb54";
+    hash = "sha256-4KZBSqjUt6s46bWBS1qh5Tc/4iHEPeBf4eBDS4WzIDA=";
   };
 
 in
@@ -20,7 +20,7 @@ in
 buildNpmPackage {
   inherit pname version src;
 
-  npmDepsHash = "sha256-9H8ip70s7PI+i/lo4zPugAYAxMD47odm0a1IZhqliRM=";
+  npmDepsHash = "sha256-ABk6AgfTQidrc3cpuKKscTD23TWs2WaaiO7Ftla02bo=";
 
   meta = {
     homepage = "https://github.com/org-formation/org-formation-cli";
