2c486add8b7ff7c1b92baca5533b8655a4055f91
David Baynard <git@baynard.dev>
2025-03-14 21:07:22 +0000

    Update caddy packages
    -   Remove `caddy-security` as it's more trouble than it's worth.
    -   Add caddy-hmac and caddy-jwt as replacements
    -   Add caddy-inspect for debugging

--- servers/caddy/caddy-extended.nix
+++ servers/caddy/caddy-extended.nix
@@ -8,9 +8,11 @@ caddyWith {
     "github.com/caddy-dns/route53" = "94b571790e968b09916dc95cc521bc1c2327f7d1";
 
     "github.com/fore-stun/spanx" = "9e9109e6b964cd36fb9cb1be8328b0f32b3d60c3";
-    "github.com/greenpau/caddy-security" = "90049c80f2c048dfc1d493c221b9f53a1dca43d5";
     "github.com/lindenlab/caddy-s3-proxy" = "850db193cb7f48546439d236f2a6de7bd7436e2e";
     "github.com/abiosoft/caddy-exec" = "521d8736cb4d1ce7f5b8bf8be6f3a2c9ecad843c";
+    "github.com/abiosoft/caddy-hmac" = "976ca0a419efb59fcbd8f32072132779e9d9a5c0";
+    "github.com/abiosoft/caddy-inspect" = "96cdb1dfb122f79913d60ecb34030d302a4f4ec1";
+    "github.com/ggicci/caddy-jwt" = "baeab7ec43c49fe77caa18b8bb16739b3276e356";
   };
-  vendorHash = "sha256-s8CGFHONXZlqDGnNZ8JCQmSk6DsApeARZShabOxB6nE=";
+  vendorHash = "sha256-oIG4rpZ+dVnTWCK1X2/vWEL9/YLyfhCxFqr0Tyg5TW4=";
 }
