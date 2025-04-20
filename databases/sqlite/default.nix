3db2dda4c860f5065df45f92845906426ebb69fe
David Baynard <git@baynard.dev>
2025-04-20 12:10:22 +0100

    Separate extra arguments to sqlite packagees

--- databases/sqlite/default.nix
+++ databases/sqlite/default.nix
@@ -4,17 +4,24 @@ let
   pnames = [ "markdown-to-sqlite" "sqlitebiter" ];
 in
 {
-  overlays.sqlite = final: prev: {
-    sqlite-extended = prev.callPackage ./package.nix {
-      inherit (final) sqlitePlugins;
-    };
-    sqlitePlugins = prev.sqlitePlugins or { }
-    // prev.callPackage ./plugins.nix { };
-  } // lib.foldFor pnames (pname: {
-    ${pname} = prev.callPackage (./. + "/${pname}.nix") {
-      inherit (final) python3Packages;
-    };
-  });
+  overlays.sqlite = final: prev:
+    let
+      extras = {
+        markdown-to-sqlite = { inherit (final) python3Packages; };
+        sqlitebiter = { inherit (final) python3Packages; };
+      };
+    in
+    {
+      sqlite-extended = prev.callPackage ./package.nix {
+        inherit (final) sqlitePlugins;
+      };
+      sqlitePlugins = prev.sqlitePlugins or { }
+      // prev.callPackage ./plugins.nix { };
+    } // lib.foldFor pnames (pname: {
+      ${pname} = prev.callPackage
+        (./. + "/${pname}.nix")
+        (extras."${pname}" or { });
+    });
 } //
 lib.foldFor lib.platforms.all (system:
   let
