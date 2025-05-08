255c918ee5d2e9ff5b530f89dd627958d1c39b36
David Baynard <git@baynard.dev>
2025-05-08 02:19:15 +0100

    Stub network/graphql

---
+++ network/graphql/default.nix
@@ -0,0 +1,21 @@
+{ self, lib, nixpkgs, ... }:
+
+let
+  pnames = [ ];
+in
+{
+  overlays.graphql = final: prev:
+    let
+      extras = { };
+    in
+    lib.foldFor pnames (pname: {
+      ${pname} = prev.callPackage
+        (./. + "/${pname}.nix")
+        (extras."${pname}" or { });
+    });
+} //
+lib.foldFor lib.platforms.all (system: {
+  packages.${system} = self.overlays.graphql
+    (nixpkgs.legacyPackages.${system} // self.packages.${system})
+    nixpkgs.legacyPackages.${system};
+})
