d8bcfa0acd2068f92065fbf1c654f28f33f48859
David Baynard <git@baynard.dev>
2025-05-26 21:21:12 +0100

    Update caddy hashes

--- servers/caddy/caddy-extended.nix
+++ servers/caddy/caddy-extended.nix
@@ -4,15 +4,15 @@
 caddy.withPlugins {
   plugins = [
     # Caddy dns
-    "github.com/caddy-dns/cloudflare@89f16b99c18ef49c8bb470a82f895bce01cbaece"
-    "github.com/caddy-dns/route53@94b571790e968b09916dc95cc521bc1c2327f7d1"
+    "github.com/caddy-dns/cloudflare@v0.2.1"
+    "github.com/fore-stun/libdns-route53@v0.0.0-20250526213255-7a723d8255bf"
 
-    "github.com/fore-stun/spanx@9e9109e6b964cd36fb9cb1be8328b0f32b3d60c3"
-    "github.com/lindenlab/caddy-s3-proxy@850db193cb7f48546439d236f2a6de7bd7436e2e"
-    "github.com/abiosoft/caddy-exec@521d8736cb4d1ce7f5b8bf8be6f3a2c9ecad843c"
-    "github.com/abiosoft/caddy-hmac@976ca0a419efb59fcbd8f32072132779e9d9a5c0"
-    "github.com/abiosoft/caddy-inspect@96cdb1dfb122f79913d60ecb34030d302a4f4ec1"
-    "github.com/ggicci/caddy-jwt@baeab7ec43c49fe77caa18b8bb16739b3276e356"
+    "github.com/fore-stun/spanx@v0.0.0-20250507102219-58d4b8a0d7f3"
+    "github.com/fore-stun/caddy-s3-proxy@v0.5.7-0.20250526214057-c90e95199238"
+    "github.com/abiosoft/caddy-hmac@v0.0.0-20210522205451-976ca0a419ef"
+    "github.com/abiosoft/caddy-exec@v0.0.0-20240914124740-521d8736cb4d"
+    "github.com/abiosoft/caddy-inspect@v0.0.0-20250214103948-96cdb1dfb122"
+    "github.com/ggicci/caddy-jwt@v0.12.0"
   ];
   hash = "sha256-fWS/DdkOcdsrRqEuh0Xlb5/PmhWd/daD1CCb5S7bHWU=";
 
