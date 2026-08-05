2a39c18d96c1121a223402e776167847f7a7ce45
David Baynard <git@baynard.dev>
2026-08-05 16:07:51 +0100

    Update caddy src hash

--- servers/caddy/caddy-extended.nix
+++ servers/caddy/caddy-extended.nix
@@ -21,7 +21,7 @@ let
       "github.com/abiosoft/caddy-inspect@v0.0.0-20250214103948-96cdb1dfb122"
       "github.com/ggicci/caddy-jwt@v0.12.0"
     ];
-    hash = "sha256-3yB9p2j7rw7aUWjh5rgMyVXrclwXFqPbs5HmQOPAhDU=";
+    hash = "sha256-qmV5NuhXEjUy6YAOAAF8ZmRfNKkeooqaKFGtTjNnZeo=";
   };
 
   withDeps = drv: drv.overrideAttrs (old: {
